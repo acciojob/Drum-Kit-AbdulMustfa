45eb37149baa726b2a63c5bfa2d2de6600454250
Abdul Mustafa <117674574+AbdulMustfa@users.noreply.github.com>
2023-06-10 13:23:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,20 @@
-//your JS code here. If required.
-function playSound(keyCode) {
-  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+window.addEventListener('keydown', playSound);
 
-  if (!audio) return;
+    function playSound(e) {
+      const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+      if (!key) return; // Exit if no corresponding key found
+      const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+      if (!audio) return; // Exit if no corresponding audio found
 
-  audio.currentTime = 0; // Rewind to the start
-  audio.play();
-  key.classList.add('playing');
-}
+      key.classList.add('playing');
+      audio.currentTime = 0;
+      audio.play();
+    }
 
-// Function to handle keydown event
-function handleKeyDown(event) {
-  const keyCode = event.keyCode;
-  playSound(keyCode);
-}
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
-// Function to remove the 'playing' class
-function removeTransition(event) {
-  if (event.propertyName !== 'transform') return;
-  this.classList.remove('playing');
-}
-
-// Add event listeners
-window.addEventListener('keydown', handleKeyDown);
-
-const keys = document.querySelectorAll('.key');
-keys.forEach((key) => {
-  key.addEventListener('transitionend', removeTransition);
-});
-In this code, we have an HTML container with the class "keys", and each keystroke button has a class "key". The data-key attribute of each key represents the respective ASCII key code. The CSS styles the layout of the keys, and the JavaScript handles the keydown event, plays the corresponding sound, and adds/removes the 'playing' class for visual effect.
-
-To make this code work, save the HTML code in an HTML file (e.g., "index.html"), the CSS code in a CSS file (e.g., "styles.css"), and the JavaScript code in a JS file (e.g., "script.js"). Place all the files in the same directory and open the HTML file in a web browser. Now, when you press the keys A, S, D, F, G, H, J, K, or L, you should hear a sound associated with each key press.
-
-Note: To make this example work, you need to have sound files associated with each key. You can add an audio element with the respective sound files and adjust the code accordingly.
-
-
-
-
-
-
+    function removeTransition(e) {
+      if (e.propertyName !== 'transform') return; // Skip if it's not a transform transition
+      this.classList.remove('playing');
+    }
